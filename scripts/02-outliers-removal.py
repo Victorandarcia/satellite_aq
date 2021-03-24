ed2ebbbd7923dcd7e01784b901e8d4de51cf5421
Victor Andarcia <iq699078@iteso.mx>
2021-03-24 13:46:40 -0600

    Removed outliers

---
+++ scripts/02-outliers-removal.py
@@ -0,0 +1,21 @@
+# Remove outliers based on the parameters specified on the outliers_removal_v2.ipynb notebook
+
+import os
+import sys
+
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+    import itesoAQ
+
+
+def main():
+
+    preprocessed_data = OutliersRemovalTools()
+    preprocessed_data.fit_data()
+    preprocessed_data.remove_std_outliers_v2()
+    preprocessed_data.export_to_csv()
+
+
+if __name__ == "__main__":
+    main()
